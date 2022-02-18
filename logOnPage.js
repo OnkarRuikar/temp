6089ba1756865df3038102070bf389d12b442574
OnkarRuikar <87750369+OnkarRuikar@users.noreply.github.com>
2022-02-18 20:41:16 +0530

    added 'Clear' button
    Added clear button at bottom right.
    It doesn't reset log number.
    Added prefix icons for warning and error logs.

--- logOnPage.js
+++ logOnPage.js
@@ -6,144 +6,169 @@
  Author: Onkar Ruikar
 */
 
- //circular stringify
-    JSON.stringifyWithCircularRefs = (function () {
-      const refs = new Map();
-      const parents = [];
-      const path = ["this"];
+//stringify handle circular refs
+JSON.stringifyWithCircularRefs = (function () {
+ const refs = new Map();
+ const parents = [];
+ const path = ['this'];
 
-      function clear() {
-        refs.clear();
-        parents.length = 0;
-        path.length = 1;
-      }
+ function clear() {
+  refs.clear();
+  parents.length = 0;
+  path.length = 1;
+ }
 
-      function updateParents(key, value) {
-        let idx = parents.length - 1;
-        let prev = parents[idx];
-        if (prev[key] === value || idx === 0) {
-          path.push(key);
-          parents.push(value);
-        } else {
-          while (idx-- >= 0) {
-            prev = parents[idx];
-            if (prev && prev[key] === value) {
-              idx += 2;
-              parents.length = idx;
-              path.length = idx;
-              --idx;
-              parents[idx] = value;
-              path[idx] = key;
-              break;
-            }
-          }
-        }
-      }
+ function updateParents(key, value) {
+  let idx = parents.length - 1;
+  let prev = parents[idx];
+  if (prev[key] === value || idx === 0) {
+   path.push(key);
+   parents.push(value);
+  } else {
+   while (idx-- >= 0) {
+    prev = parents[idx];
+    if (prev && prev[key] === value) {
+     idx += 2;
+     parents.length = idx;
+     path.length = idx;
+     --idx;
+     parents[idx] = value;
+     path[idx] = key;
+     break;
+    }
+   }
+  }
+ }
 
-      function checkCircular(key, value) {
-        if (value != null) {
-          if (typeof value === "object") {
-            if (key) { updateParents(key, value); }
+ function checkCircular(key, value) {
+  if (value != null) {
+   if (typeof value === 'object') {
+    if (key) {
+     updateParents(key, value);
+    }
 
-            let other = refs.get(value);
-            if (other) {
-              return '[Circular Reference]' + other;
-            } else {
-              refs.set(value, path.join('.'));
-            }
-          }
-        }
-        return value;
-      }
+    let other = refs.get(value);
+    if (other) {
+     return '[Circular Reference]' + other;
+    } else {
+     refs.set(value, path.join('.'));
+    }
+   }
+  }
+  return value;
+ }
 
-      return function stringifyWithCircularRefs(obj, space) {
-        try {
-          parents.push(obj);
-          return JSON.stringify(obj, checkCircular, space);
-        } finally {
-          clear();
-        }
-      }
-    })();
+ return function stringifyWithCircularRefs(obj, space) {
+  try {
+   parents.push(obj);
+   return JSON.stringify(obj, checkCircular, space);
+  } finally {
+   clear();
+  }
+ };
+})();
 
-    // patch logs
-    (function () {
-      let log = console.log;
-      let warn = console.warn;
-      let error = console.error;
-      let debug = console.debug;
-      let logger = document.getElementById('pageLog');
-      if (!logger) {
-        let logDiv = `
+// patch logs
+(function () {
+ const log = console.log;
+ const warn = console.warn;
+ const error = console.error;
+ const debug = console.debug;
+ let logger = document.getElementById('pageLog');
+ if (!logger) {
+  const logDiv = `
               <style>
                 #pageLog *{
                   margin:0;
                   padding:0;
                 }
               </style>
-              <div id="pageLog" style="height: 50vh; 
+              <div id='pageLog' style='height: 50vh; 
 							overflow:auto;
 							width: 100%;
               font-size:0.7rem;
-							background-color:#bbb"></div>`;
-        document.body.insertAdjacentHTML('beforeend', logDiv);
-        logger = document.getElementById('pageLog');
-      }
+              position:relative;
+							background-color:#ddd'>
+                <div id='clearPageLog'
+                   onclick='clearPageLogs()'
+                   style='
+                   border: 1px dotted darkred;
+                   position:absolute;
+                   bottom: .5rem;
+                   right: 1rem;
+                   cursor:pointer;
+                   '>Clear</div>
+              </div>`;
+  document.body.insertAdjacentHTML('beforeend', logDiv);
+  logger = document.getElementById('pageLog');
+ }
 
-      let count = 0;
-      function myLog() {
-        let text = `<span style='color:${this.color}'>${count++}: `;
-        for (var i = 0; i < arguments.length; i++) {
-          if (typeof arguments[i] == 'object') {
-            text += (JSON && JSON.stringify ? JSON.stringifyWithCircularRefs(arguments[i], undefined, 2) : arguments[i]) + '<br />';
-          } else {
-            text += arguments[i] + '<br />';
-          }
-        }
-        text += `</span>`;
-        logger.insertAdjacentHTML('beforeend', `${text}`);
-      }
+ window.clearPageLogs = function () {
+  let clear = clearPageLog;
+  pageLog.innerHTML = '';
+  pageLog.appendChild(clear);
+  console.log('logging on page');
+ };
 
-      console.log = (...args) => {
-        let obj = { color: 'black', myLog: myLog };
-        obj.myLog(...args);
-        log(...args);
-      }
-      console.warn = (...args) => {
-        let obj = { color: 'brown', myLog: myLog };
-        obj.myLog(...args);
-        warn(...args);
-      }
-      console.error = (...args) => {
-        let obj = { color: 'red', myLog: myLog };
-        obj.myLog(...args);
-        error(...args);
-      }
-      console.debug = (...args) => {
-        let obj = { color: '#555', myLog: myLog };
-        obj.myLog(...args);
-        debug(...args);
-      }
+ let count = 0;
+ function myLog() {
+  let text = `<span style='color:${this.color}'>${count++}: `;
+  for (var i = 0; i < arguments.length; i++) {
+   if (typeof arguments[i] == 'object') {
+    text +=
+     (JSON && JSON.stringify
+      ? JSON.stringifyWithCircularRefs(arguments[i], undefined, 2)
+      : arguments[i]) + ' ';
+   } else {
+    text += arguments[i] + ' ';
+   }
+  }
+  text += `</span><br/>`;
+  logger.insertAdjacentHTML('beforeend', `${text}`);
+ }
 
-      //handle global errors
-      window.onerror = function (msg, url, lineNo, columnNo, error) {
-				var string = msg.toLowerCase();
-				var message = [
-					'Message: ' + msg,
-					'URL: ' + url,
-					'Line: ' + lineNo,
-					'Column: ' + columnNo,
-					'Error object: ' + JSON.stringifyWithCircularRefs(error)
-				].join(' - ');
-				console.error0(message);
-				return false;
-			};
+ console.log = (...args) => {
+  let obj = { color: 'black', myLog: myLog };
+  obj.myLog(...args);
+  log(...args);
+ };
+ console.warn = (...args) => {
+  let obj = { color: '#6d5001', myLog: myLog };
+  obj.myLog('&#9888;', ...args);
+  warn(...args);
+ };
+ console.error = (...args) => {
+  let obj = { color: 'darkred', myLog: myLog };
+  obj.myLog('&#10060;', ...args);
+  error(...args);
+ };
+ console.debug = (...args) => {
+  let obj = { color: '#666', myLog: myLog };
+  obj.myLog(...args);
+  debug(...args);
+ };
 
-			window.onunhandledrejection = function (event) {
-				console.error(`Unhandled Promise Rejection. Reason: 
+ //handle global errors
+ window.onerror = function (msg, url, lineNo, columnNo, error) {
+  var string = msg.toLowerCase();
+  var message = [
+   'Message: ' + msg,
+   'URL: ' + url,
+   'Line: ' + lineNo,
+   'Column: ' + columnNo,
+   'Error object: ' + JSON.stringifyWithCircularRefs(error),
+  ].join(' - ');
+  console.error0(message);
+  return false;
+ };
+
+ window.onunhandledrejection = function (event) {
+  console.error(
+   `Unhandled Promise Rejection. Reason: 
           ${JSON.stringifyWithCircularRefs(event.reason)}`,
-					` Returne value: ${event.returnValue}`);
-			};
-      
-      console.log('logging on page');
-    })();
+   ` Returne value: ${event.returnValue}`
+  );
+ };
+
+ console.log('logging on page');
+})();
